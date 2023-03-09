c2722e4b23802bc7f2b8a7ac8ce179b4884a4a89
GursimranKaur3452 <123474785+GursimranKaur3452@users.noreply.github.com>
2023-03-09 22:33:48 +0530

    Add files via upload

---
+++ cat123.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+marks=int(input("enter the marks"))
+if marks>85 and marks<=100:
+                print("congrats yu score a grade")
+elif marks>75 and marks<=85:
+                print("congrats yu score b grade")
+elif marks>65 and marks<=75:
+                print("congrats yu score c grade")
+elif marks >32 and marks<=65:
+                print("yu score d grade")
+else:
+                print("sorry yu are fail")
+    
+
+
+# In[8]:
+
+
+number=int(input("enter the number"))
+if number%2==0:
+    print("num is even")
+    if number==0:
+        print("num is zero")
+else :
+    print("num is odd")
+
+
+# In[9]:
+
+
+n=int(input())
+m=int(input())
+if n%2==0:
+    if m%2==0:
+        print("1")
+    else :
+        print("2")
+else:
+    print("3")
+
+
+# In[3]:
+
+
+a=1
+while a<=10:
+    print("ram")
+    a=a+1
+print("end")
+
+
+# In[ ]:
+
+
+
+
